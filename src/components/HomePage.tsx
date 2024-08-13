2d20628cb25e574b57551b4c7285af016f0d8a1e
Richard Uggelberg <Richard.Uggelberg@samhall.se>
2024-08-13 20:36:26 +0200

    fix home page image size

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,4 +1,4 @@
-import AsdImage from "../../public/asd.jpeg"; // Adjust the path as needed
+import SunsetImage from "../../public/sunset.jpg"; // Adjust the path as needed
 
 const HomePage = () => {
   return (
@@ -10,20 +10,29 @@ const HomePage = () => {
           alignItems: "center",
         }}
       >
-        <h1 style={{ marginBottom: "10px" }}>Vi gifter oss!</h1>{" "}
-        {/* Banner text */}
+        <h1 style={{ marginBottom: "10px" }}>Vi gifter oss!</h1>
         <p style={{ fontSize: "16px", marginBottom: "20px", color: "#555" }}>
-          {" "}
-          {/* Smaller text for date and time */}
           31 Maj 2025, kl. 15:00
         </p>
-        <img
-          src={AsdImage}
-          alt="Asd"
-          style={{ width: "100%", height: "auto", maxWidth: "100%" }} // Ensures the image fits within the container while maintaining aspect ratio
-        />
+        <div
+          style={{
+            width: "100%",
+            maxWidth: "700px", // Set max-width for wider screens
+            border: "2px solid #ccc", // Add a nice border
+            overflow: "hidden", // Ensure the border-radius applies to the image
+          }}
+        >
+          <img
+            src={SunsetImage}
+            alt="Sunset"
+            style={{
+              width: "100%",
+              height: "auto",
+              display: "block",
+            }} // Image should fill the container while maintaining aspect ratio
+          />
+        </div>
       </div>
-      <div style={{ display: "flex", justifyContent: "center" }}>asd</div>
     </>
   );
 };
